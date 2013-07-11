6fe763e6d9d4be1b073ff859529867e05f6c21a1
dnorton <daniel@dnorton.org>
2013-07-11 12:25:05 -0400

    Create snippets.py

---
+++ scripts/snippets.py
@@ -0,0 +1,7 @@
+"""a collection of interesting python snippets"""
+
+# just to satisfy my curiousity, I tried to figure out how to print 1 to 10 in python
+# This is a fairly convoluted way to do it. Note, in 2.x print is a statement
+# so I had to import the 3.x print() function
+from __future__ import print_function
+map(lambda x: print(x+1), range(10))
